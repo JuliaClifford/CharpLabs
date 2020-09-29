8c695ee12424194c94c479b20deb1a271598e461
JuliaClifford <69593248+JuliaClifford@users.noreply.github.com>
2020-09-29 23:39:54 +0300

    L2

---
+++ L2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace L2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+
+            try
+            {
+                Console.Write("Введите стартовое значение для произведения ряда: ");
+                input = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
+            int first = input * input;
+            int finish = first * input;
+
+            double result = 1; 
+
+            if (0 <= first && first <= finish)
+            {
+                for (int k = first; k < finish; k++)
+                {
+                    result *= (3*k + Math.Pow((-1), Math.Pow(k, 2) - k + 1) * k) / (2 * Math.Pow(k, 2) + 1);
+                }
+                Console.WriteLine($"Результат исчесления произведения ряда с стартовым числом {input} = {result}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+                Console.ReadKey();
+            }
+        }
+    }
+    
+}
